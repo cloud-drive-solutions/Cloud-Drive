f00c7a281401587ce17e59a7e1a1334f232e0f2c
jpesquetini <jpesquetinif@gmail.com>
2022-06-18 05:29:52 -0600

    Inicios de la base de datos

--- webapi/src/index.js
+++ webapi/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 //Configuration
+const app = express();
 app.set('port', process.env.PORT || 3000);
 
 //Middle configuration
@@ -13,6 +16,18 @@ app.use(cors());
 
 app.use(require('./routes/index'));
 app.use('/info/' , require('./routes/info'));
+
+//Connecting to Data Base
+const uri = 'mongodb+srv://Sapos_Datos2:DanielSapoHP@cluster0.ryokwe7.mongodb.net/?retryWrites=true';
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Connected to the Data Base')
+})
+
+const userRouter = require('./routes/user');
+
+app.use('/user', userRouter);
 
 //Start server
 app.listen(app.get('port'), () => {
